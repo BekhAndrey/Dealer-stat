d3af03cc42eab64a01102175d751a2b45c321f57
BekhAndrey <andreyac17@gmail.com>
2021-11-28 17:15:22 +0300

    Created GameObject model

---
+++ src/main/java/com/bekh/dealerstat/model/GameObject.java
@@ -0,0 +1,31 @@
+package com.bekh.dealerstat.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "game_object")
+public class GameObject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User user;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt = LocalDate.now();
+
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "game_id", nullable = false)
+    private Game game;
+}

266f43a1437edae591c5c7286e5bb7600558bb40
BekhAndrey <andreyac17@gmail.com>
2021-11-28 14:02:44 +0300

    Added validation on user fields

--- src/main/java/com/bekh/dealerstat/model/User.java
+++ src/main/java/com/bekh/dealerstat/model/User.java
@@ -3,6 +3,10 @@ package com.bekh.dealerstat.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
@@ -11,14 +15,28 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "first_name")
+    @NotBlank(message = "First Name cannot be empty.")
+    @Size(min = 2, max = 25, message = "First Name cannot be less than 2 and more than 25 symbols")
     private String firstName;
+
     @Column(name = "last_name")
+    @NotBlank(message = "Last Name cannot be empty.")
+    @Size(min = 2, max = 25, message = "Last Name cannot be less than 2 and more than 25 symbols")
     private String lastName;
+
+    @Email(message = "Invalid email template.")
+    @NotBlank(message = "Email cannot be empty.")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty.")
+    @Size(min = 8, message = "Password cannot be less than 8 symbols")
     private String password;
+
     @Column(name = "created_at")
-    private LocalDate creationDate;
+    private LocalDate creationDate = LocalDate.now();
+
     @Enumerated(EnumType.STRING)
     private UserRole role;
 }

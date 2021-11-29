c981dade4d18dfd8525fa4ec617771739c5947e5
BekhAndrey <andreyac17@gmail.com>
2021-11-29 17:04:49 +0300

    Updated findById implementation

--- src/main/java/com/bekh/dealerstat/service/GameService.java
+++ src/main/java/com/bekh/dealerstat/service/GameService.java
@@ -3,7 +3,9 @@ package com.bekh.dealerstat.service;
 import com.bekh.dealerstat.model.Game;
 import com.bekh.dealerstat.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,9 +18,11 @@ public class GameService {
         return  (List<Game>) gameRepository.findAll();
     }
 
-    public Game findById(Long id) { return gameRepository.findById(id).orElse(null);}
+    public Game findById(Long id) { return gameRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find the resource"));}
 
     public void save(Game game) { gameRepository.save(game);}
 
     public void delete(Game game) { gameRepository.delete(game);}
+
+    public Game findByName(String name) { return gameRepository.findByName(name);}
 }

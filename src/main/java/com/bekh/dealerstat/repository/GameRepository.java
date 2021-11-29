1f40592ba74c50bd151daadbd9e21485df21d208
BekhAndrey <andreyac17@gmail.com>
2021-11-29 17:03:59 +0300

    Added findByName query

--- src/main/java/com/bekh/dealerstat/repository/GameRepository.java
+++ src/main/java/com/bekh/dealerstat/repository/GameRepository.java
@@ -3,5 +3,8 @@ package com.bekh.dealerstat.repository;
 import com.bekh.dealerstat.model.Game;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.validation.constraints.NotBlank;
+
 public interface GameRepository extends CrudRepository<Game, Long> {
+    Game findByName(String name);
 }

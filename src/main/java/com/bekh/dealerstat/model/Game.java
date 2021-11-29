274f57c7eb89b6f311f7a5c7b9206aa7d59d2d21
BekhAndrey <andreyac17@gmail.com>
2021-11-29 17:02:56 +0300

    Added validation

--- src/main/java/com/bekh/dealerstat/model/Game.java
+++ src/main/java/com/bekh/dealerstat/model/Game.java
@@ -2,6 +2,7 @@ package com.bekh.dealerstat.model;
 
 import lombok.Data;
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Set;
 
 @Entity
@@ -10,9 +11,10 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Name cannot be empty.")
     private String name;
 
-    @OneToMany(mappedBy = "game", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "game", fetch = FetchType.EAGER)
     private Set<GameObject> gameObjects;
 }
 
